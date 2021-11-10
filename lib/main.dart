ee32d62e68caa0d5bb93c7a1a4a5acfcd4788363
Lucas Andrade <lucasandrade595@gmail.com>
2021-11-10 18:51:44 -0300

    Feat: Inicio.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bytebank/components/theme.dart';
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: bytebankTheme,
+      home: Dashboard(),
+    );
+  }
+}
